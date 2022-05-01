328937dc64a65005116de3098e72d83a1279969e
Claudia M <iseanow@gmail.com>
2022-04-30 21:58:38 -0400

    Added COOOL contract to my branch.

---
+++ GroupNFT/contracts/CoolCoin.sol
@@ -0,0 +1,12 @@
+
+
+pragma solidity ^0.5.5;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract COOOL is ERC20, ERC20Detailed {
+    constructor() ERC20Detailed("Coool", "CL", 4) public {
+        _mint(msg.sender, 100000000);
+    }
+}
